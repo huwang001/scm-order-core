dfe4251921600217db558e772d4cc86276609b81
huwang <13657262083@139.com>
2020-08-12 19:36:50 +0800

    init

---
+++ src/test/java/com/example/scmordercore/ScmOrderCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scmordercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmOrderCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
